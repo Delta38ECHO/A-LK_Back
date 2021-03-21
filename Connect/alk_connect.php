0526ec9b4c8c8c2557b548912f8216256e222b42
MattVal <60153736+Delta38ECHO@users.noreply.github.com>
2021-03-20 17:29:00 -0700

    Attempting htaccess fix to see errors
    .PHP files are broken and aren't displaying errors. Attempting to gain access to .htaccess file to turn error reporting on.  ...I hate .php...

--- Connect/alk_connect.php
+++ Connect/alk_connect.php
@@ -1,19 +1,40 @@
 <?php
-	//conect_to_database
+//
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+//
+//conect_to_database
 $server = 'bender.cs.csubak.edu';
 $user = 'noay';
 $password = 'ALK4321!';
 $database = 'alk';
-$db = new mysqli($server, $user, $password, $database);
-// oop   $obj->member
- if($db->connect_error) {
-     exit("Error 501");
- }
- else {
-     // DEVELOPMENT
-     echo "Success<br>";
-}
+$db = mysqli($server, $user, $password, $database)
+or die('Error connecting to MySQL server.');
 
 //Bring up a connection to the server for ALK only. This is to test connectivity.
-
 ?>
+
+<html>
+ <head>
+ </head>
+ <body>
+ <h1>PHP connect to MySQL</h1>
+ 
+ <?php
+//Step2
+$query = "SELECT * FROM Accounts";
+mysqli_query($db, $query) or die('Error querying database.');
+
+$result = mysqli_query($db, $query);
+$row = mysqli_fetch_array($result);
+
+while ($row = mysqli_fetch_array($result)) {
+ echo $row['Account_ID'] . ' ' . $row['Rasp_ID'] . ' ' . $row['fName'] . ' ' . $row['lName'] . ' ' .$row['cName'] . ' ' .  $row['email'] . ' ' . $row['password'] .'<br />';
+}
+?>
+
+</body>
+</html>
