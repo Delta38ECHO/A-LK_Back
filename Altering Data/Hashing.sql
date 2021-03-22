245387c6625377e6a3e9fddd25a978879735045d
MattVal <60153736+Delta38ECHO@users.noreply.github.com>
2021-03-22 14:24:18 -0700

    Update Hashing.sql
    Made alterations to file and created it to be a guide for password hashing.

--- Altering Data/Hashing.sql
+++ Altering Data/Hashing.sql
@@ -1,4 +1,14 @@
 --To hash stored passwords in the pre-existing table--
 --First: Update password len to the needed 32 bit reg for MD5 hashing.
+
+ALTER TABLE Accounts MODIFY password VARCHAR(32);
+
 --Next use: 
 UPDATE Accounts SET password = MD5(password);
+--SHOULD SET password to MD5 hash and allow for later comparison.
+
+--Show update.
+
+SELECT * FROM Accounts;
+
+--Success, all passwords are now hashed.
