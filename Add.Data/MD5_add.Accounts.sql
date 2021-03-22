2c1d270e9976c968684681f44f51f37b97d2e976
MattVal <60153736+Delta38ECHO@users.noreply.github.com>
2021-03-22 16:44:22 -0700

    Broke the passwords...
    Yea yea yea...I messed up...I'm going to be trying to get them back somehow.  Had to change the password value to length 224.
    
    Now to update all 50+ passwords.

---
+++ Add.Data/MD5_add.Accounts.sql
@@ -0,0 +1,5 @@
+insert into Accounts (Account_ID, Rasp_ID, fName, lName, cName, email, password) values (51, 102, 'Ermin', 'Maneylaws', 'Jones-Watsica', 'emaneylaws0@dot.gov', '8tLf2UDI85');
+insert into Accounts (Account_ID, Rasp_ID, fName, lName, cName, email, password) values (52, 104, 'Beatrisa', 'Brandassi', null, 'bbrandassi1@wisc.edu', 'I7x9fwI');
+insert into Accounts (Account_ID, Rasp_ID, fName, lName, cName, email, password) values (53, 106, 'Fiann', 'Andric', null, 'fandric2@people.com.cn', 'fPUhNKcue');
+insert into Accounts (Account_ID, Rasp_ID, fName, lName, cName, email, password) values (54, 108, 'Rahal', 'Burkhill', null, 'rburkhill3@biblegateway.com', 'jU0FXS6');
+insert into Accounts (Account_ID, Rasp_ID, fName, lName, cName, email, password) values (55, 110, 'Harriot', 'Shervil', 'Rolfson Inc', 'hshervil4@engadget.com', 'GSn2vRpNsC7m');
--- Altering Data/Hashing.sql
+++ Altering Data/Hashing.sql
@@ -1,11 +1,11 @@
 --To hash stored passwords in the pre-existing table--
---First: Update password len to the needed 32 bit reg for MD5 hashing.
+--First: Update password len to the needed 32 bit reg for SHA2 hashing.
 
-ALTER TABLE Accounts MODIFY password VARCHAR(32);
+ALTER TABLE Accounts MODIFY password VARCHAR(224);
 
 --Next use: 
-UPDATE Accounts SET password = MD5(password);
---SHOULD SET password to MD5 hash and allow for later comparison.
+UPDATE Accounts SET password = SHA2(password, 224);
+--SHOULD SET password to SHA2 hash and allow for later comparison.
 
 --Show update.
 
@@ -14,10 +14,19 @@ SELECT * FROM Accounts;
 --Success, all passwords are now hashed.
 
 --Does it always hash a new password?  No...
---Change the type of Password in Accounts to be a password hashed into MD5.
+--Change the type of Password in Accounts to be a password hashed into SHA2.
 
 --UPDATE `thistable` SET `thistable`.`column1` = `md5`
 --WHERE `thistable`.`column1` = `currentvalue` 
 
-UPDATE Accounts SET password = md5(password);
+--UPDATE Accounts SET password = md5(password);
+
+Update Accounts set password = SHA2(password, 224);
+
 --Success!!  The SHOW FIELDS shows that password type is now varchar32 and will be hashed in md5 automatically. 
+
+--Show warnings;
+
+
+--The first SHA2 depicted all passwords to NULL. Must update the passwords ID: 1-50
+--to a new password.
