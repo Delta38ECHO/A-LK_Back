ea970791189d6eaa546ca67f9325d1dcf4437467
MattVal <60153736+Delta38ECHO@users.noreply.github.com>
2021-04-10 16:46:09 -0700

    Still Altering DB
    Co-Authored-By: gfuentesherr <60154823+gfuentesherr@users.noreply.github.com>

--- Altering Data/pass_update.sql
+++ Altering Data/pass_update.sql
@@ -11,3 +11,9 @@ SET FOREIGN_KEY_CHECKS=1; --On
 --Turn on FK constraints again.
 
 --Use Hashing.sql again to make passwords into SHA2.
+
+---Also note that the:
+
+--SHOW TABLES;
+
+--command is a good way of showing all existing tables in the DB.
