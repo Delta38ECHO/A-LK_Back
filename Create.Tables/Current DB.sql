dfa82163c81646cb53e57a05f4f5ee0d76d806c5
MattVal <60153736+Delta38ECHO@users.noreply.github.com>
2021-04-12 15:16:18 -0700

    Still Altering DB
    can't have two foreign keys in DB WITHOUT special SQL instructions. Those are being made in the "Current_DB" file.

--- Create.Tables/Current DB.sql
+++ Create.Tables/Current DB.sql
@@ -16,14 +16,35 @@ CREATE TABLE Users
   Email VARCHAR(40) NOT NULL,
   Password VARCHAR(270) NOT NULL,
   Zest VARCHAR(15) NOT NULL,
-  Authority INT(4) NOT NULL
+  Authority INT(4) NOT NULL,
 );
 
 CREATE TABLE u_Access
 ( Access_ID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  User_ID VARCHAR(20) NOT NULL,
+  Master_ID VARCHAR(20) NOT NULL,
+  Mas_Car_ID INT(20) UNSIGNED NOT NULL,
+  Slave_ID VARCHAR(20) NOT NULL,
   Acc_op INT(1) NOT NULL,
-  FOREIGN KEY (User_ID) REFERENCES Users (User_ID)
+  --FOREIGN KEY (User_ID) REFERENCES Users (User_ID)
+  /*
+  So this needs a revamp;  The table will be rebuilt to be co-cat. with Users table.
+  Master uses primary User_ID to then ALLOW Slave to use Vehicle "Mas_Car_ID" owned by Master.
+  */
+);
+
+--Table: u_Access won't be used, use the new one below:
+CREATE TABLE u_Access.Users(
+	Access_ID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	Master_ID VARCHAR(20) NOT NULL,
+	Mas_Car_ID INT(20) UNSIGNED NOT NULL,
+	Slave_ID VARCHAR(20) NOT NULL,
+	Acc_op INT(1) NOT NULL,
+	INDEX 'Master_ID_key_idx' ('Master_ID' ASC),
+	INDEX 'Mas_Car_ID_key_idx' ('Mas_Car_ID' ASC),
+	INDEX 'Slave_ID_key_idx' ('Slave_ID' ASC),
+	CONSTRAINT '' FOREIGN KEY ('') REFERENCES ``.`` (``) ON DELETE NO ACTION ON UPDATE NO ACTION,
+	CONSTRAINT '' FOREIGN KEY ('') REFERENCES ``.`` (``) ON DELETE NO ACTION ON UPDATE NO ACTION,
+	CONSTRAINT '' FOREIGN KEY ('') REFERENCES ``.`` (``) ON DELETE NO ACTION ON UPDATE NO ACTION,
 );
 
 CREATE TABLE Logs
@@ -34,3 +55,38 @@ CREATE TABLE Logs
   FOREIGN KEY (User_ID) REFERENCES Users (User_ID),
   FOREIGN KEY (Car_ID) REFERENCES Cars (Car_ID)
 );
+
+/*
+
+	For the working parts of the DB, the Co_ID NEEDS to be the grouping aspect of u_Access table. A user is created with their own Co_ID that then gives them the ability to link other users via their User_ID in table Users.
+	
+	This is then stored in a linked table known as u_Access where it stores the User_ID. There's a MASTER_ID Where the car owner/account owner is listed and then there's the SLAVE_ID Where Master allows slave to use vehicle owned by master. 
+	
+	Example:
+	CREATE TABLE `test`.`user` (
+	`iduser` INT NOT NULL,
+	`username` VARCHAR(45) NULL,
+	PRIMARY KEY (`iduser`));
+
+
+CREATE TABLE `test`.`transfer` (
+  `transactionID` INT NOT NULL,
+  `from_user` INT NULL,
+  `to_user` INT NULL,
+  `transfer_amount` FLOAT NULL,
+  PRIMARY KEY (`transactionID`),
+  INDEX `from_user_key_idx` (`from_user` ASC),
+  INDEX `to_user_key_idx` (`to_user` ASC),
+  CONSTRAINT `from_user_key`
+    FOREIGN KEY (`from_user`)
+    REFERENCES `test`.`user` (`iduser`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `to_user_key`
+    FOREIGN KEY (`to_user`)
+    REFERENCES `test`.`user` (`iduser`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+	
+
+*/
